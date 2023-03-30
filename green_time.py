846dfa6977267c316bccbd54e9b4da1c94517045
AshishPeterRajeev <codehub.ash@gmail.com>
2023-03-30 12:06:37 +0530

    initial commit

---
+++ green_time.py
@@ -0,0 +1,25 @@
+
+import time
+import random
+
+
+carTime = 2
+bikeTime = 1
+bicycleTime = 1
+# rickshawTime = 2.25 
+busTime = 2.5
+truckTime = 2.5
+
+defaultRed = 60
+defaultYellow = 5
+defaultGreen = 45
+defaultMinimum = 10
+defaultMaximum = 60
+
+currentGreen = 0
+
+# vehicle_count = 21
+
+vehicle_count = random.randint(0,20)
+
+
